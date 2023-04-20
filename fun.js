1b683959e4de216353dddcea650845d70cf079d9
Tyler Byrne <byrnetyl@msu.edu>
2023-04-20 13:57:57 -0400

    java test

--- fun.js
+++ fun.js
@@ -1,4 +1,4 @@
 function displayInput() {
-    var name = document.getElementById("input").ariaValueMax;
+    var name = document.getElementById("input").value;
     document.getElementById("output").innerHTML = "Hello, " + name +"!";
 }
